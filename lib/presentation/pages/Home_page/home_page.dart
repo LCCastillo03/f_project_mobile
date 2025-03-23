b96c443df62c853d460d5d9cd252bfd336a8174c
Edgar Garcia <aedavila@uninorte.edu.co>
2025-03-23 02:59:41 -0500

    Generalized event details page, Added controllers, Added interactivity for event details page, Added Event Model, Modified Home Page

--- lib/presentation/pages/Home_page/home_page.dart
+++ lib/presentation/pages/Home_page/home_page.dart
@@ -1,173 +1,139 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart'; 
-import '../../widgets/search_item.dart' ;
-import '../../widgets/event_card.dart'; 
-
+import 'package:provider/provider.dart';
+import '../../widgets/search_item.dart';
+import '../../widgets/event_card.dart';
+import 'package:project/controllers/events_controller.dart';
+import 'package:project/controllers/subscription_controller.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 250, 250, 250),
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              _buildHeader(),
-              const SizedBox(height: 16),
-              const Padding(
-                padding: EdgeInsets.only(left: 20),
-                child: Text(
-                  'Live Event',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
+    return ChangeNotifierProvider(
+      create: (context) => SubscriptionController(),
+      child: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 250, 250, 250),
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                _buildHeader(),
+                const SizedBox(height: 16),
+                const Padding(
+                  padding: EdgeInsets.only(left: 20),
+                  child: Text(
+                    'Live Event',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(height: 16),
-
-              _buildEventTarget(),
-              
-            ],
+                const SizedBox(height: 16),
+                _buildEventTarget(context),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 
-Widget _buildHeader() {
-  String formattedDate = DateFormat('dd MMMM yyyy').format(DateTime.now()); // Fecha actual
-
-  return Stack(
-    children: [
-      Container(
-        width: double.infinity,
-        color: Color(0xFFFEF2EE),
-        padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 20  ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            SizedBox(height: 40),
-            Text(
-              'Schedule Event',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
+  Widget _buildHeader() {
+    return Stack(
+      children: [
+        Container(
+          width: double.infinity,
+          color: const Color(0xFFFEF2EE),
+          padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 40),
+              const Text(
+                'Schedule Event',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            SizedBox(height: 4),
-            Text(
-              formattedDate,
-              style: TextStyle(
-                color: Colors.black54,
-                fontSize: 16,
+              const SizedBox(height: 4),
+              SearchItem(
+                onChanged: (value) {},
               ),
-            ),
-            SizedBox(height: 10), 
-            SearchItem(
-              onChanged: (value) {
-
-              },
-            ), 
-          ],
-        ),
-      ),
-      Positioned(
-        top: 0,
-        right: 0,
-        child: SizedBox(
-          height: 150,
-          child: Image.asset(
-            "assets/images/Deco_home.png",
-            fit: BoxFit.cover,
+            ],
           ),
         ),
-      ),
-    ],
-  );
-} 
+      ],
+    );
+  }
 
+  Widget _buildEventTarget(BuildContext context) {
+    List<Map<String, String>> events = [
+      {
+        "imagePath": "assets/images/laterns-beach.png",
+        "month": "Dec",
+        "day": "24",
+        "eventName": "Mindfulness Workshop",
+        "organizer": "Marlon Piñeres",
+        "distance": "18.2",
+        "location": "Angeles City",
+        "route": "laterns",
+      },
+      {
+        "imagePath": "assets/images/stars.png",
+        "month": "Jan",
+        "day": "16",
+        "eventName": "Engineering Future",
+        "organizer": "Elias Niño",
+        "distance": "10.5",
+        "location": "New York",
+        "route": "engine",
+      },
+      {
+        "imagePath": "assets/images/atomic.png",
+        "month": "Jun",
+        "day": "16",
+        "eventName": "Future of Learning",
+        "organizer": "Miguel Jimeno",
+        "distance": "25.8",
+        "location": "Los Angeles",
+        "route": "education",
+      },
+      {
+        "imagePath": "assets/images/buildings.png",
+        "month": "Jun",
+        "day": "12",
+        "eventName": "FinTech Revolution",
+        "organizer": "Augusto Salazar",
+        "distance": "30.2",
+        "location": "San Francisco",
+        "route": "business",
+      },
+    ];
 
-Widget _buildEventTarget() {
-  List<Map<String, String>> events = [
-    {
-      "imagePath": "assets/images/laterns-beach.png",
-      "month": "Dec",
-      "day": "24",
-      "eventName": "Lantern Festival",
-      "organizer": "Suku Zhong",
-      "distance": "18.2",
-      "location": "Angeles City",
-    },
-    {
-      "imagePath":"assets/images/laterns-beach.png",
-      "month": "Jan",
-      "day": "01",
-      "eventName": "New Year Fireworks",
-      "organizer": "City Council",
-      "distance": "10.5",
-      "location": "New York",
-    },
-    {
-      "imagePath": "assets/images/laterns-beach.png",
-      "month": "Jul",
-      "day": "15",
-      "eventName": "Summer Music Fest",
-      "organizer": "Live Nation",
-      "distance": "25.8",
-      "location": "Los Angeles",
-    },
-  ];
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        bool isWeb = constraints.maxWidth > 600;
 
-return LayoutBuilder(
-  builder: (context, constraints) {
-    bool isWeb = constraints.maxWidth > 600; 
-
-    if (isWeb ) {
-    
-      return SizedBox(
-        height: 800,
-        child: GridView.builder(
-          scrollDirection: Axis.vertical,
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 3, 
-            crossAxisSpacing: 5,
-            mainAxisSpacing: 5,
-            childAspectRatio: 0.8, 
-          ),
-          itemCount: events.length,
-          itemBuilder: (context, index) {
-            return EventCard(
-              imagePath: events[index]["imagePath"]!,
-              month: events[index]["month"]!,
-              day: events[index]["day"]!,
-              eventName: events[index]["eventName"]!,
-              organizer: events[index]["organizer"]!,
-              distance: events[index]["distance"]!,
-              location: events[index]["location"]!,
-            );
-          },
-        ),
-      );
-    } else {
-   
-      return SizedBox(
-        height: 380,
-        child: PageView.builder(
-          controller: PageController(viewportFraction: 0.75),
-          itemCount: events.length,
-          itemBuilder: (context, index) {
-            return Center(
-              child: SizedBox(
-                width: constraints.maxWidth * 0.7,
-                child: EventCard(
+        if (isWeb) {
+          return SizedBox(
+            height: 800,
+            child: GridView.builder(
+              scrollDirection: Axis.vertical,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                crossAxisSpacing: 5,
+                mainAxisSpacing: 5,
+                childAspectRatio: 0.8,
+              ),
+              itemCount: events.length,
+              itemBuilder: (context, index) {
+                return EventCard(
                   imagePath: events[index]["imagePath"]!,
                   month: events[index]["month"]!,
                   day: events[index]["day"]!,
@@ -175,18 +141,40 @@ return LayoutBuilder(
                   organizer: events[index]["organizer"]!,
                   distance: events[index]["distance"]!,
                   location: events[index]["location"]!,
-                ),
-              ),
-            );
-          },
-        ),
-      );
-    }
-  },
-);
-
-
+                  onTap: () => EventsController.navigateTo(
+                      context, events[index]["route"]!),
+                );
+              },
+            ),
+          );
+        } else {
+          return SizedBox(
+            height: 380,
+            child: PageView.builder(
+              controller: PageController(viewportFraction: 0.75),
+              itemCount: events.length,
+              itemBuilder: (context, index) {
+                return Center(
+                  child: SizedBox(
+                    width: constraints.maxWidth * 0.7,
+                    child: EventCard(
+                      imagePath: events[index]["imagePath"]!,
+                      month: events[index]["month"]!,
+                      day: events[index]["day"]!,
+                      eventName: events[index]["eventName"]!,
+                      organizer: events[index]["organizer"]!,
+                      distance: events[index]["distance"]!,
+                      location: events[index]["location"]!,
+                      onTap: () => EventsController.navigateTo(
+                          context, events[index]["route"]!),
+                    ),
+                  ),
+                );
+              },
+            ),
+          );
+        }
+      },
+    );
+  }
 }
-
-}
-
--- lib/presentation/widgets/event_card.dart
+++ lib/presentation/widgets/event_card.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'event_details.dart';
 
-class EventCard extends StatelessWidget {
+class EventCard extends StatefulWidget {
   final String imagePath;
   final String month;
   final String day;
@@ -9,6 +8,8 @@ class EventCard extends StatelessWidget {
   final String organizer;
   final String distance;
   final String location;
+  final VoidCallback onTap;
+  final bool isInitiallySubscribed;
 
   const EventCard({
     super.key,
@@ -19,29 +20,53 @@ class EventCard extends StatelessWidget {
     required this.organizer,
     required this.distance,
     required this.location,
+    required this.onTap,
+    this.isInitiallySubscribed = false,
   });
 
   @override
+  _EventCardState createState() => _EventCardState();
+}
+
+class _EventCardState extends State<EventCard> {
+  late bool isSubscribed;
+
+  @override
+  void initState() {
+    super.initState();
+    isSubscribed = widget.isInitiallySubscribed;
+  }
+
+  void toggleSubscription() {
+    setState(() {
+      isSubscribed = !isSubscribed;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Center(
+    return GestureDetector(
+      onTap: widget.onTap,
       child: Container(
         width: 300,
         height: 360,
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(20),
           image: DecorationImage(
-            image: AssetImage(imagePath),
+            image: AssetImage(widget.imagePath),
             fit: BoxFit.cover,
             alignment: Alignment.center,
           ),
         ),
         child: Stack(
           children: [
+            // 📌 Fecha del evento
             Positioned(
               top: 15,
               left: 15,
               child: Container(
-                padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 2),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 10, vertical: 2),
                 decoration: BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.circular(10),
@@ -49,14 +74,14 @@ class EventCard extends StatelessWidget {
                 child: Column(
                   children: [
                     Text(
-                      month,
+                      widget.month,
                       style: const TextStyle(
                         color: Colors.black87,
                         fontSize: 14,
                       ),
                     ),
                     Text(
-                      day,
+                      widget.day,
                       style: const TextStyle(
                         color: Color.fromARGB(207, 72, 64, 222),
                         fontSize: 20,
@@ -67,11 +92,80 @@ class EventCard extends StatelessWidget {
                 ),
               ),
             ),
-            EventDetails(
-              eventName: eventName,
-              organizer: organizer,
-              distance: distance,
-              location: location,
+            // 📌 Detalles del evento con el corazón a la derecha del nombre
+            Positioned(
+              bottom: 20,
+              left: 10,
+              right: 10,
+              child: Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // 📌 Fila con el nombre del evento y el corazón
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Expanded(
+                          child: Text(
+                            widget.eventName,
+                            style: const TextStyle(
+                              color: Colors.black87,
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: toggleSubscription,
+                          child: Icon(
+                            isSubscribed
+                                ? Icons.favorite
+                                : Icons.favorite_border,
+                            color: Colors.purple, // Corazón morado
+                            size: 24,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 4),
+                    Text(
+                      'By ${widget.organizer}',
+                      style: const TextStyle(
+                        color: Colors.black54,
+                        fontSize: 14,
+                      ),
+                    ),
+                    const SizedBox(height: 4),
+                    // 📌 Ubicación y distancia
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          widget.location,
+                          style: const TextStyle(
+                            color: Colors.black54,
+                            fontSize: 12,
+                          ),
+                        ),
+                        Text(
+                          '${widget.distance} km away',
+                          style: const TextStyle(
+                            color: Colors.black54,
+                            fontSize: 12,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
             ),
           ],
         ),
